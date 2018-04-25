380f8a56834163bc9eb001f48e000dd55c076728
Zarddy <zarddy@iMac.local>
2018-04-25 23:38:27 +0800

    为打包成war发布到tomcat进行调整
    1、将默认打包格式改为war
    2、引入spring-boot-starter-tomcat，scope设为provided
    3、调整Application类

--- src/main/java/cn/imiaomi/admin/api/ApiApplication.java
+++ src/main/java/cn/imiaomi/admin/api/ApiApplication.java
@@ -3,10 +3,17 @@ package cn.imiaomi.admin.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan(basePackages = "cn.imiaomi.admin.api.mapper")
-public class ApiApplication {
+public class ApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
